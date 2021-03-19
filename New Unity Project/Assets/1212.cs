723e428210a1a353782af8508d9ef69c98680afb
linuschan1025 <200280451@stu.vtc.edu.hk>
2021-03-19 13:13:56 +0900

    updated script

--- New Unity Project/Assets/1212.cs
+++ New Unity Project/Assets/1212.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ] : MonoBehaviour
+public class 1212 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
